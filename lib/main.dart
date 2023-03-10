9738699320a4473218e6eb240f3c8876edd394f5
Dirk Taylor <dirktay@durham.net>
2023-03-10 17:59:57 -0500

    Append project - customization test!

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,73 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: MyHomePage(),
+          child: MyOtherPage(),
         ),
       ),
+    );
+  }
+}
+
+class MyOtherPage extends StatefulWidget {
+  const MyOtherPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyOtherPage> createState() => _MyOtherPageState();
+}
+
+class _MyOtherPageState extends State<MyOtherPage> {
+  void playSound(Source what) async {
+    final player = AudioPlayer();
+    await player.play(what);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: TextButton(
+            // style: TextButton.styleFrom(padding: EdgeInsets.zero),
+            onPressed: () =>
+                playSound(AssetSource('01176-wooden-windbells-on-wind-8.wav')),
+            child: Container(
+              // width: 300,
+              color: Colors.lightBlue,
+              child: Center(
+                child: Text(
+                  'Chimes',
+                  style: TextStyle(color: Colors.blue.shade900),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            // style: TextButton.styleFrom(padding: EdgeInsets.zero),
+            onPressed: () {
+              // playSound(UrlSource('https://freesound.org/s/416529/'));
+              playSound(AssetSource(
+                  '416529__inspectorj__bird-whistling-single-robin-a.wav'));
+            },
+            child: Container(
+              // width: 300,
+              color: Colors.lightGreen,
+              child: Center(
+                child: Text(
+                  'Robin chirp',
+                  style: TextStyle(color: Colors.blue.shade900),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const Expanded(
+          flex: 7,
+          child: MyHomePage(),
+        ),
+      ],
     );
   }
 }
@@ -28,8 +92,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // final player = AudioPlayer();
-  // static AudioCache player = AudioCache();a
   List myColors = [
     Colors.red,
     Colors.orange,
@@ -37,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Colors.green,
     Colors.teal,
     Colors.blue,
-    Colors.purple,
+    Colors.purple
   ];
 
   void playNote(int index) async {
@@ -48,9 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget aKey(int index) {
     return Expanded(
       child: TextButton(
-        style: TextButton.styleFrom(
-          padding: EdgeInsets.zero,
-        ),
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
         onPressed: () => playNote(index + 1),
         child: Container(
           color: myColors[index],
